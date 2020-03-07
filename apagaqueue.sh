0dd64f755986c10979d58833f1887cc660d0f576
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-06 22:00:50 -0300

    Update apagaqueue.sh

--- apagaqueue.sh
+++ apagaqueue.sh
@@ -1,3 +1,19 @@
-service exim stop
+#!/bin/bash
+###############################################################################
+# Copyright 2020							      
+#Author: Fagner Mendes							     
+#License: GNU Public License						      
+#Version: 1.0								      
+# Email: fagner.mendes22@gmail.com					     
+###############################################################################
+
+echo "Stoping the Exim Service..."
+sleep 2
+/scripts/restart_srv exim --stop
+echo "Preparing to delete the server queue..."
+eleep 2
 find /var/spool/exim/input -type f -exec rm -f {} +
-service exim start
+echo "Initializing the Exim Server..."
+/scripts/restart_srv exim --start
+echo "The process was done!"
+done
