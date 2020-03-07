56bce9d8de0385ece5f9136596710db7081c802d
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-06 22:49:46 -0300

    Create inodesearch.sh

---
+++ inodesearch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+###############################################################################
+# Copyright 2020							                                                
+# Author: Fagner Mendes							                                          
+# License: GNU Public License						                                      
+# Version: 1.0								                                                  
+# Email: fagner.mendes22@gmail.com					                                  
+###############################################################################
+# This is script search Inode usage by cPanel account in the system
+
+echo "Inode usage for: $(pwd)" ; for d in `find -maxdepth 1 -type d |cut -d\/ -f2 |grep -xv . |sort`; do c=$(find $d |wc -l) ; printf "$c\t\t- $d\n" ; done ; printf "Total: \t\t$(find $(pwd) | wc -l)\n"
