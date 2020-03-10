352baf15d96c716a2a80151d1d58c47049125730
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-10 12:01:53 -0300

    Update lpcp.sh

--- lpcp.sh
+++ lpcp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ###############################################################################
-# Copyright 2020							                                                
-# Author: Fagner Mendes							                                          
-# License: GNU Public License						                                      
-# Version: 1.0								                                                  
-# Email: fagner.mendes22@gmail.com					                                  
+# Copyright 2020
+# Author: Fagner Mendes
+# License: GNU Public License
+# Version: 1.1
+# Email: fagner.mendes22@gmail.com
 ###############################################################################
 
 #This script list the email account and change the password for a default pass
@@ -14,8 +14,10 @@
 echo "Starting the list process email accounts and change passwords"
 sleep 5
 
-uapi list_pops --user=LOGINCPANEL | grep ' - ' | sed -e 's/ - //' > /root/LOGIN_emails.txt
-for email in $(cat /root/LOGIN_emails.txt) ; do uapi --user=contacpanel Email passwd_pop email=$email password=senha domain=dominiodacontas ; do
+whmapi1 list_pops_for user=LOGINCPANEL | grep ' - ' | sed -e 's/ - //' > /root/suporte_emails.txt
+for email in $(cat /root/suporte_emails.txt) ; do uapi --user=LOGINCPANEL Email passwd_pop email=$email password=SENHA domain=DOMINIO
 sleep 5
-
+done
 echo "The password was changed for all accounts"
+
+
