75cb206a0838f5c495e638afdbea0057c147debb
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-10 12:35:43 -0300

    Update updatecs.sh

--- updatecs.sh
+++ updatecs.sh
@@ -3,7 +3,7 @@
 # Copyright 2020							     
 #Author: Fagner Mendes							      
 #License: GNU Public License						     
-#Version: 1.0								      
+#Version: 1.1								      
 # Email: fagner.mendes22@gmail.com
 ###############################################################################
 
@@ -11,6 +11,6 @@
 
 echo "Starting the update process..."
 sleep 2
-curl -s download.configserver.com/csupdate | perl > /tmp/update-configserver.txt
+curl -s download.configserver.com/csupdate > /tmp/update-configserver.txt
 echo "The configserver plugin was updated!"
 done
