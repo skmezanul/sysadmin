060eb012a9b786b16687b94e29b0332d0c985322
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-09 22:21:40 -0300

    Create lpcp.sh
    lpcp = List pops and change passwords

---
+++ lpcp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+###############################################################################
+# Copyright 2020							                                                
+# Author: Fagner Mendes							                                          
+# License: GNU Public License						                                      
+# Version: 1.0								                                                  
+# Email: fagner.mendes22@gmail.com					                                  
+###############################################################################
+
+#This script list the email account and change the password for a default pass
+
+
+echo "Starting the list process email accounts and change passwords"
+sleep 5
+uapi list_pops --user=LOGINCPANEL | grep ' - ' | sed -e 's/ - //' > /root/LOGIN_emails.txt
+for email in $(cat /root/LOGIN_emails.txt) ; do uapi --user=contacpanel Email passwd_pop email=$email password=senha domain=dominiodacontas ; do
+echo "The passwords was changed for all accounts"
