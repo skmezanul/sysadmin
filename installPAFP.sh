ae45f7ffa7bb8aa0bf0b1cbe1d6feefe6118a515
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-11 23:38:39 -0300

    Update installPAFP.sh

--- installPAFP.sh
+++ installPAFP.sh
@@ -24,11 +24,14 @@ echo "Done!"
 echo "Set the PPA"
 LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 echo "Starting the installation PHP 7.3"
-apt install php7.3 php7.3-cli php7.3-common -y
+apt install php7.3 php7.3-cli php7.3-common y
 echo "Done!"
 echo Starting process stallation PHP extesions"
-apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl -y
+apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl php-pgsql php3-pgsql -y
 echo "Done!"
+echo "Enabling the pdo module"
+a2enmod php3-pgsql
+echo "The module was enable"
 sleep 5
 echo "Starting the stallation of vsftpd"
 apt install vsftpd -y
