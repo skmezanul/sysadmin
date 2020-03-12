c193acc8de274d73cfd5980408aa0ea393ae5465
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-11 23:32:04 -0300

    Update recoveryroundcube.sh

--- recoveryroundcube.sh
+++ recoveryroundcube.sh
@@ -1,3 +1,14 @@
 !#/bin/bash
+
+###############################################################################
+# Copyright 2020							                                                
+# Author: Fagner Mendes							                                          
+# License: GNU Public License						                                      
+# Version: 1.1								                                                  
+# Email: fagner.mendes22@gmail.com					                                  
+###############################################################################
+
+# This script can be repair the roundcube rpm and fix errors
+
 rpm -e --nodeps cpanel-roundcubemail
 /usr/local/cpanel/scripts/check_cpanel_rpms --fix
