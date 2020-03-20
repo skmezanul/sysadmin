159ced037f360c06922b75b7271ef895ae2f224e
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-20 16:47:52 -0300

    Update spacemonitor.sh

--- spacemonitor.sh
+++ spacemonitor.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 ######
-#Daniel Padovani - 14 set 2010
+########Daniel Padovani - 14 set 2010
 ######
 #A Linha abaixo limpa o arquivo antes de utilizado
 touch /dev/null > /root/bkp/usohd.txt
@@ -13,7 +13,7 @@ USO=`df -h / | tail -1 | awk '{print $5}'| sed "s/%//g"`
 #enviado a notificacao atraves do email que o seu HD ultrapassou o persentual.
 if [ "$USO" -gt "85" ]; then
 #altere onde esta escrito /SUAPARTICAO para a partição que deseja monitorar
-echo "Particao / esta com "$USO"% de uso! Favor verificar!" >> /root/bkp/usohd.txt
+echo "Particao / do servidor '$HOSTNAME' esta com "$USO"% de uso! Favor verificar!" >> /root/bkp/usohd.txt
 #else touch /root/bkp/usohd.txt
 fi
 #echo `cat /root/bkp/usohd.txt`
@@ -22,5 +22,5 @@ if [ ! -s /root/bkp/usohd.txt ] ;then
 echo "Arquivo esta vazio!"
 else
 #altere a linha abaixo aonde esta escrito seu@email.com.br pelo seu email correto
-cat /root/bkp/usohd.txt | mail -s '[Utilizacao do HD HOSTNAMESERVIDOR]'
+cat /root/bkp/usohd.txt | mail -s "[ Utilizacao do HD em "$(hostname)" ]" EMAIL
 fi
