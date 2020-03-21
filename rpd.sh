5160c79c86f2c4f2cc12232e8226591a3229da0f
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-21 19:49:32 -0300

    Update rpd.sh

--- rpd.sh
+++ rpd.sh
@@ -4,10 +4,9 @@
 # Copyright 2020							                                                
 # Author: Fagner Mendes							                                          
 # License: GNU Public License						                                      
-# Version: 1.2								                                                  
+# Version: 1.3							                                                  
 # Email: fagner.mendes22@gmail.com					                                  
 ###############################################################################
-
 
 package-cleanup --dupes | tail -n +3 > duplist.txt
 cat duplist.txt | sort --version-sort | awk 'NR % 2 {print "rpm -e --justdb --nodeps " $1 } !(NR % 2) {match($0, "-[0-9]");print "yum -y reinstall " substr($0,0,RSTART-1)}'
@@ -15,4 +14,3 @@ cat duplist.txt | sort --version-sort | awk 'NR % 2 {print "rpm -e --justdb --no
 #expect "Do you want to continue?" { send "\r" }
 echo "Starting the process to solve this issue, please wait!"
 cat duplist.txt | sort --version-sort | awk 'NR % 2 {print "rpm -e --justdb --nodeps " $1 } !(NR % 2) {match($0, "-[0-9]");print "yum -y reinstall " substr($0,0,RSTART-1)}' | sh
-
