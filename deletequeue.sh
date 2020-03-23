c74ead91724b6e3e4eca8751b240fe236c44676f
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-23 16:04:12 -0300

    Update deletequeue.sh

--- deletequeue.sh
+++ deletequeue.sh
@@ -3,16 +3,18 @@
 # Copyright 2020							      
 # Author: Fagner Mendes							     
 # License: GNU Public License						      
-# Version: 1.0								      
+# Version: 1.1								      
 # Email: fagner.mendes22@gmail.com					     
 ###############################################################################
 
 echo "Stoping the Exim Service..."
 sleep 2
 /scripts/restartsrv_exim --stop
+clear
 echo "Preparing to delete the server queue..."
 sleep 2
 find /var/spool/exim/input -type f -exec rm -f {} +
+clear
 echo "Starting the Exim Server..."
 /scripts/restartsrv_exim --start
 echo "The process was done!"
