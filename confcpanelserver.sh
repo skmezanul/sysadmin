1480aad985489d95e0bd8466e6ca07490850aa37
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-01 16:59:43 -0300

    Update confcpanelserver.sh

--- confcpanelserver.sh
+++ confcpanelserver.sh
@@ -8,46 +8,52 @@
 # Email: fagner.mendes22@gmail.com					                                  
 ###############################################################################
 
+<< 'CHANGELOG'
+
+1.1 - 01/04/2020 [Author: Fagner Mendes]
+#Changes
+- Change the functions (Disable functions, change permissions, )
+
+CHANGELOG
+
+echo ""
+
+
 echo "Starting the update server"
 yum update -y
 echo "Done..."
 
 /scripts/upcp --force
 
-sed "s/IPADDR=/IPADDR=66.147.225.40/g /etc/sysconfig/network-scripts/ifcfg-ens3
+sed -i 's/IPADDR=216.120.234.31/IPADDR=216.120.234.31/g' /etc/sysconfig/network-scripts/ifcfg-ens3
 
-echo "Disable Functions"
 
-/sbin/chkconfig cups off 
-/sbin/service nfslock stop
-/sbin/chkconfig nfslock off
-/sbin/service rpcidmapd stop 
-/sbin/chkconfig rpcidmapd off
-/sbin/service bluetooth stop
-/sbin/chkconfig bluetooth off
-/sbin/service anacron stop
-/sbin/chkconfig anacron off
-/sbin/service hidd stop
-/sbin/chkconfig hidd off
-/sbin/service pcscd stop
-/sbin/chkconfig pcscd off
-/sbin/service avahi-daemon stop
-/sbin/chkconfig avahi-daemon off
+echo "Prepare to disable functions in the server"
+for SERVICE in autofs cups nfslock rpcidmapd rpcidmapd bluetooth anacron hidd pcscd avahi-daemon
+do
+        service "$SERVICE" stop > /dev/null 2>&1
+        chkconfig "$SERVICE" off > /dev/null 2>&1
+done
 echo "Done..."
+clear
+sleep 5
 
-sleep 2
+echo "Prepare to changes the permission files"
+for FILES1 in /usr/bin/rcp /usr/bin/rcp /usr/bin/lynx /usr/bin/links /usr/bin/scp /usr/bin/gcc
+do
 
-echo "Change the permissions for any files"
-chmod 750 /usr/bin/rcp
-chmod 750 /usr/bin/wget
-chmod 750 /usr/bin/lynx
-chmod 750 /usr/bin/links
-chmod 750 /usr/bin/scp
-chmod 000 /etc/httpd/proxy/
-chmod 000 /var/spool/samba/
-chmod 000 /var/mail/vbox/
+chmod 750 "$FILES1" > /dev/null 2>&1
+done
 
-sleep 2
+for FILES2 in /etc/httpd/proxy/ /var/spool/samba/ /var/mail/vbox/
+do
+
+chmod 000 "$FILES2" > /dev/null 2>&1
+done
+
+echo "Done"
+
+clear
 
 echo "Install DNS Check"
 
@@ -56,27 +62,27 @@ wget http://download.ndchost.com/accountdnscheck/latest-accountdnscheck
 sh latest-accountdnscheck
 echo "Done..."
 
-sleep 2
+sleep 5
 
 echo "Adding script remove"
 echo "30 23 * * * sh /root/remover.sh" >> /var/spool/cron/root
 chmod 755 /root/remover.sh
 echo "Done..."
 
-sleep 2
+sleep 5
 
 echo "Runnig the fix quotas"
 /scripts/fixquotas
 echo "Done..."
 
-sleep 2
+sleep 5
 
 echo "Apache monitor, please wait while set the permission"
-sleep 2
+sleep 5
 chmod 755 /root/cron/http.sh
 echo "00 * * * * sh /root/cron/http.sh" >> /var/spool/cron/root
 echo "Done..."
-sleep 2
+sleep 5
 
 echo "Change the Apache port"
 sed "s/apache_port=/apache_port=0.0.0.0:80/g /var/cpanel/cpanel.config
@@ -93,16 +99,17 @@ yum install apachetop -y
 yum install mytop -y
 yum install nethogs
 yum install nload -y
+yum update --disablerepo=epel
 echo "Done..."
 cd /root/
 wget https://raw.githubusercontent.com/fagner-fmlo/arquivos/master/.mytop
 echo "Mytop is configured"
-sleep 2
+sleep 5
 
 echo "Disabling Selinux"
 sed s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
 echo "Done..."
-sleep 2
+sleep 5
 
 echo "Add script for update configserver"
 cd /root/
