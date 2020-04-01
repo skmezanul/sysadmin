19ea2a61d91e4b1992a989a68ef5aaed9769b728
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-01 17:32:12 -0300

    Update roudcubebase.sh

--- roudcubebase.sh
+++ roudcubebase.sh
@@ -4,8 +4,28 @@
 # Copyright 2020							                                                
 # Author: Fagner Mendes							                                          
 # License: GNU Public License						                                      
-# Version: 1.0								                                                  
+# Version: 1.1								                                                  
 # Email: fagner.mendes22@gmail.com					                                  
 ###############################################################################
+
 # This script verify the roundcube database is sqllite or mysql, for migration is necessary that servers has the same database.
-grep -i roundcu /var/cpanel/cpanel.config
+
+<< 'CHANGELOG'
+1.1 - 01/04/2020 [Author: Fagner Mendes, help from Felipe Rangel]
+#Changes
+- Reformulate the script
+
+CHANGELOG
+
+echo ""
+
+#!/bin/bash
+
+ROUNDCUBE=`/usr/bin/grep -i roundcube_db /var/cpanel/cpanel.config`
+if [ "${ROUNDCUBE}" == "${ROUNDCUBE}" ]; then
+       echo "DB is Sqlite, nothing to do"
+    else
+        echo "Prepare to convert from mysql to sqlite"
+/usr/local/cpanel/scripts/convert_roundcube_mysql2sqlite
+echo "Done"
+    fi
