23d911b2862843d0f402541dbb30d0e106172195
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-03 17:17:03 -0300

    Update createhooksspf.sh

--- createhooksspf.sh
+++ createhooksspf.sh
@@ -15,6 +15,7 @@ cd /var/cpanel/zonetemplates/
 mv /var/cpanel/zonetemplates/root_standardvirtualftp /var/cpanel/zonetemplates/root_standardvirtualftp-bkp
 echo "Done"
 sleep 5
+clear
 echo "Downloadong the new file, please wait"
 wget https://raw.githubusercontent.com/fagner-fmlo/arquivos/master/root_standardvirtualftp
 chmod 644 root_standardvirtualftp
@@ -22,13 +23,13 @@ clear
 sleep 5
 bash <( curl -s https://raw.githubusercontent.com/fagner-fmlo/sysadmin/master/createdir.sh)
 echo "Done"
+clear
 cd /var/cpanel/perl5/lib/
 wget https://raw.githubusercontent.com/fagner-fmlo/arquivos/master/Spf.pm
 sleep 5
+clear
 echo "Prepare to install module Spf"
 /usr/local/cpanel/bin/manage_hooks add module Spf
 sleep 5
 /scripts/upcp 2&1> /root/upcp.log
-grep "Copy" /usr/local/cpanel/logs/error_log
-grep "Replace" /usr/local/cpanel/logs/error_log
-grep "Delete" /usr/local/cpanel/logs/error_log
+echo "Done"
