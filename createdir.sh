c466468fb4af932d2a2a4204592d570361b0c274
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-03 11:09:56 -0300

    Update createdir.sh

--- createdir.sh
+++ createdir.sh
@@ -4,9 +4,15 @@
 # Copyright 2020							                                                
 # Author: Fagner Mendes							                                          
 # License: GNU Public License						                                      
-# Version: 1.0								                                                  
+# Version: 1.1								                                                  
 # Email: fagner.mendes22@gmail.com					                                  
 ###############################################################################
+
+<< 'CHANGELOG'
+1.1 - 03/04/2020 [Author: Fagner Mendes]
+#Changes
+- Was added another if to compare a another direcetory
+CHANGELOG
 
 
 DIR=`ls /var/cpanel/perl5/lib/`
@@ -16,6 +22,15 @@ echo "Directory exist"
 else
 echo "Directory is not exist, do you want to create it?"
 read
-CRIA=`mkdir /var/cpanel/perl5/lib/`
+CRIATE=`mkdir /var/cpanel/perl5/lib/`
+echo "Directory created"
+fi
+
+DIR2=`ls /usr/local/cpanel/base/root_standardvirtualftp/`
+if [ -e "/usr/local/cpanel/base/root_standardvirtualftp/" ]; then
+echo "Directory exist"
+else
+echo "Direcotory is not exist, do you want to create it?"
+CRIATE=`mkdir -p /usr/local/cpanel/base/root_standardvirtualftp/`
 echo "Directory created"
 fi
