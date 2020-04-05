270fb375ecf37d7b3ef1bee81c26ba0feae2c9b8
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-05 15:56:03 -0300

    Create checkrdns.sh

---
+++ checkrdns.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+###############################################################################
+# Copyright 2020							                                                
+# Author: Fagner Mendes							                                          
+# License: GNU Public License						                                      
+# Version: 1.0								                                                  
+# Email: fagner.mendes22@gmail.com					                                  
+###############################################################################
+
+
+#This script is a loop for massive querys of rDNS
+
+echo ""
+
+IPS=$( cat /root/host.txt )
+for i in $IPS; do dig -x $i +short
+done
