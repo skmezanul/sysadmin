f5585d70bd33a30c6bab7597e86d72619f451251
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-06 17:44:56 -0300

    Create sa-learn2.sh

---
+++ sa-learn2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###############################################################################
+# Copyright 2020							                                                
+# Author: Fagner Mendes							                                          
+# License: GNU Public License						                                      
+# Version: 1.0								                                                  
+# Email: fagner.mendes22@gmail.com					                                  
+###############################################################################
+
+# This script do the search in all mail boxe and reports to the SA learn what is spam
+
+
+echo ""
+
+#Prepare to reset the informations SA"
+/usr/local/cpanel/3rdparty/bin/sa-learn --clear
+/usr/local/cpanel/3rdparty/bin/sa-learn --sync
+echo "Done"
+clear
+
+echo ""
+
+echo "Prepare to start SA to all account in the server"
+/usr/local/cpanel/3rdparty/bin/sa-learn -p /home/*/.spamassassin/user_prefs --use-ignores --spam /home/*/mail/*/*/.spam/{cur,new}
+echo " The process was done"
+
+echo ""
+
+clear
+
