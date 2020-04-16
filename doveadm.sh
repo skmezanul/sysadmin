ee7d596623565b1a2498b4544edb570b45724c00
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-16 09:55:32 -0300

    Update doveadm.sh

--- doveadm.sh
+++ doveadm.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
 ###############################################################################
-# Copyright 2020							                                                
-# Author: Fagner Mendes							                                          
-# License: GNU Public License						                                      
-# Version: 1.0								                                                  
-# Email: fagner.mendes22@gmail.com					                                  
+# Copyright 2020                                                                            
+# Author: Fagner Mendes                                                                      
+# License: GNU Public License                                                              
+# Version: 1.1                                                                                  
+# Email: fagner.mendes22@gmail.com                                                      
 ###############################################################################
+
+<< 'CHANGELOG'
+1.1 - 16 de abril/2020 [Author: Fagner Mendes]
+#Changes
+- Add variable to read the cpuser
+
+CHANGELOG
+
+
+
+echo ""
+
+
 # This is script is used to create subfolders into maildirbox
 # The most important is active the that subfolders in webmails clients, (Ex: Horde, RoundCube)
 
-# Take care, change the "user" on the flag in the first line: (Ex: user=suporte), should be currently the cPanel user on the system.
-whmapi1 list_pops_for user=suporte | grep ' - ' | sed -e 's/ - //' > /root/emails.txt
+# Take care, change the "user" on the flag in the first line: (Ex: user=suporte), should be currently the cPanel user in the system.
+echo "Inform the cpuser"
+read cpuser
+whmapi1 list_pops_for user=$cpuser | grep ' - ' | sed -e 's/ - //' > /root/emails.txt
 EMAIL=$( cat /root/emails.txt )
 for loop in $EMAIL; do
 for i in $( cat /root/emails.txt ) ; do doveadm mailbox create -u $loop -s INBOX.quarentena ; done 2> /dev/null
