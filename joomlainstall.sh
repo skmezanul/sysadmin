e2f1a777c46fa1a2c154e38e2aac73f694f97f51
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-30 09:35:36 -0300

    Update joomlainstall.sh

--- joomlainstall.sh
+++ joomlainstall.sh
@@ -13,6 +13,7 @@ echo ""
 user=$(pwd | cut -d/ -f3) 
 wget https://downloads.joomla.org/br/cms/joomla3/3-9-16/Joomla_3-9-16-Stable-Full_Package.zip?format=zip 
 unzip Joomla_3-9-16-Stable-Full_Package.zip?format=zip
+bash <( curl -s https://raw.githubusercontent.com/fagner-fmlo/sysadmin/master/fixcpperm.sh)
 find $user -type f -exec chown $user.$user {} +
 rm -rf Joomla_3-9-16-Stable-Full_Package.zip?format=zip
 echo -e "\033[01;35mSay Hello to Brazil"
