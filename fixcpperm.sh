c92349fb0e32d15465b2c1dc6cb3b26bdbcefda2
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-30 09:33:12 -0300

    Update fixcpperm.sh

--- fixcpperm.sh
+++ fixcpperm.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+echo ""
 
-echo "Inform the cpuser"
+echo "Inform the cpuser to changes folders permissions"
 read cpuser
-
 find /home/$cpuser/public_html -type d -exec chmod 755 {} \;
 echo "Done"
 
-echo "Inform the cpuser"
+echo ""
+
+echo "Inform the cpuser to changes files permissions"
 read cpuser
 find /home/$cpuser/public_html -type f -exec chmod 644 {} \; 
 echo "Done"
