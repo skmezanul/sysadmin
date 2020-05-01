324f9031dd8dee470809dcea9e1f8cf7e5faf52f
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-05-01 15:04:20 -0300

    Update engintroninstall.sh

--- engintroninstall.sh
+++ engintroninstall.sh
@@ -1,3 +1,9 @@
+#!/bin/bash 
+
+echo ""
+
+echo "Prepare to install Engintron in this server"
+
 cd /  
 rm -f engintron.sh  
 wget --no-check-certificate https://raw.githubusercontent.com/engintron/engintron/master/engintron.sh  
