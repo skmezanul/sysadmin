75824bfcba0208bae363e2eb2667c9c8db1db31e
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-05-02 11:23:29 -0300

    Update installPAFP.sh

--- installPAFP.sh
+++ installPAFP.sh
@@ -27,7 +27,7 @@ echo "Starting the installation PHP 7.3"
 apt install php7.3 php7.3-cli php7.3-common y
 echo "Done!"
 echo Starting process stallation PHP extesions"
-apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl php-pgsql php3-pgsql -y
+apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl php-pgsql php3-pgsql -y
 echo "Done!"
 echo "Enabling the pdo module"
 a2enmod php3-pgsql
