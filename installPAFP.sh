fb8f56a108e0cd21733a2f555bd682d62799b3ae
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-05-02 13:17:02 -0300

    Update installPAFP.sh

--- installPAFP.sh
+++ installPAFP.sh
@@ -37,7 +37,7 @@ echo "Done!"
 echo ""
 
 echo "Starting process stallation PHP extesions"
-apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl php-pgsql php3-pgsql -y
+apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl php-pgsql php3-pgsql php7.0-pgsql php7.3-pgsql php7.4-pgsql -y
 echo "Done!"
 
 echo ""
