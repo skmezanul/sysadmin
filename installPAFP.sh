b8a0f60ec618ef7561a48c98a50bd2dabb63eeac
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-05-02 11:50:28 -0300

    Update installPAFP.sh

--- installPAFP.sh
+++ installPAFP.sh
@@ -33,7 +33,10 @@ LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 echo "Starting the installation PHP 7.3"
 apt install php7.3 php7.3-cli php7.3-common -y
 echo "Done!"
-echo Starting process stallation PHP extesions"
+
+echo ""
+
+echo "Starting process stallation PHP extesions"
 apt install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl php-pgsql php3-pgsql -y
 echo "Done!"
 
