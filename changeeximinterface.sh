df8f1dca26f8b4d5c9802e3c2e0ec7380c2f37fa
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-05-04 17:56:44 -0300

    Create changeeximinterface.sh

---
+++ changeeximinterface.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+###############################################################################
+# Copyright 2020							                                                
+# Author: Fagner Mendes							                                          
+# License: GNU Public License						                                      
+# Version: 1.0								                                                  
+# Email: fagner.mendes22@gmail.com					                                  
+###############################################################################
+
+
+echo ""
+
+echo "Inform the ipaddress"
+read ipaddress
+/scripts/restartsrv_exim --stop
+sed -i 's/interface =.*/interface = $ipaddress/g' /etc/exim.conf
+sed -i 's/helo_data =.*/helo_data = ndns30524.dizinc.com/g' /etc/exim.conf
+/scripts/buildeximconf
+/scripts/restartsrv_exim --start
+echo "Done"
