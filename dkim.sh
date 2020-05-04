f77dcb9ab80e0f6d80250a79f401a775911e5de5
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-05-04 09:23:41 -0300

    Create dkim.sh

---
+++ dkim.sh
@@ -0,0 +1,3 @@
+#!/bin/ash
+
+for user in `cat /etc/userdomains | cut -d" " -f2 | sort| uniq`; do /usr/local/cpanel/bin/dkim_keys_install $user > /dev/null 2>&1 ; done ; rm -rf /var/spool/exim/input/*
