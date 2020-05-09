dabf8af97597eba89c0c605347d3028738de137f
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-05-09 12:35:14 -0300

    Update mysqlproc.sh

--- mysqlproc.sh
+++ mysqlproc.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+###############################################################################
+# Copyright 2020                                                                            
+# Author: Fagner Mendes                                                                      
+# License: GNU Public License                                                              
+# Version: 1.1                                                                                  
+# Email: fagner.mendes22@gmail.com                                                      
+###############################################################################
+
+echo ""
+
+
 mysqladmin proc | grep "Waiting for table level lock" > /root/mysqlproc.txt
 MYSQLPROC=$( cat /root/mysqlproc.txt )
 for loop in $MYSQLPROC; do
