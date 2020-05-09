7b38abc6954d5f5964e16ec190bfcfcbdf8a1cf7
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-05-09 12:09:52 -0300

    Update mysqlproc.sh

--- mysqlproc.sh
+++ mysqlproc.sh
@@ -1,4 +1,4 @@
 mysqladmin proc | grep "Waiting for table level lock" | wc -l >> /root/mysqlproc.txt
-for proc in $( cat /root/mysqlproc.txt ) ; do mysqladmin proc
+for proc in $( cat /root/mysqlproc.txt ) ; do mysqladmin proc stat
 done
 
