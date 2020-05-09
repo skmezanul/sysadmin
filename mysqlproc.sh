2aaa5105dc2447d24078a3acc431695b9fc6cecc
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-05-09 12:27:42 -0300

    Update mysqlproc.sh

--- mysqlproc.sh
+++ mysqlproc.sh
@@ -1,6 +1,6 @@
 mysqladmin proc > /root/mysqlproc.txt
 MYSQLPROC=$( cat /root/mysqlproc.txt )
 for loop in $MYSQLPROC; do
-for i in $( cat /root/mysqlproc.txt ) ; do mysqladmin proc stat $loop ; done
+for i in $( cat /root/mysqlproc.txt ) ; do mysqladmin proc $loop ; done
 done
 
