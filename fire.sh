9c3d64e285838b382e5b7bda8257f2fb4175ee41
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-06-09 16:46:20 -0300

    Update fire.sh

--- fire.sh
+++ fire.sh
@@ -2,13 +2,13 @@
 
 echo ""
 
-echo "Statrting the process"
+echo "Starting the process"
 
 cd /etc/
 mv hosts.allow hosts.allow-bkp2
 mv hosts.deny hosts.deny-bk2
-wget http://arquivos.servhost.com.br/hosts.allow --http-user=romero --http-passwd=servhost84@!
-wget http://arquivos.servhost.com.br/hosts.deny --http-user=romero --http-passwd=servhost84@!
+wget http://arquivos.servhost.com.br/vps/hosts.allow --http-user=romero --http-passwd=servhost84@!
+wget http://arquivos.servhost.com.br/vps/hosts.deny --http-user=romero --http-passwd=servhost84@!
 chown root.root hosts.*
 chmod 644 hosts.*
 
@@ -17,9 +17,9 @@ cd /etc/csf/
 mv csf.conf csf.conf-bkp
 mv csf.allow csf.allow-bkp2
 mv csf.deny csf.deny-bkp2
-wget http://arquivos.servhost.com.br/csf.conf --http-user=romero --http-passwd=servhost84@!
-wget http://arquivos.servhost.com.br/csf.allow --http-user=romero --http-passwd=servhost84@!
-wget http://arquivos.servhost.com.br/csf.deny --http-user=romero --http-passwd=servhost84@!
+wget http://arquivos.servhost.com.br/vps/csf.conf --http-user=romero --http-passwd=servhost84@!
+wget http://arquivos.servhost.com.br/vps/csf.allow --http-user=romero --http-passwd=servhost84@!
+wget http://arquivos.servhost.com.br/vps/csf.deny --http-user=romero --http-passwd=servhost84@!
 chown root.root csf.conf
 chown root.root csf.allow
 chowm root.root csf.deny
@@ -28,3 +28,10 @@ chmod 644 csf.conf csf.allow csf.deny
 bash configserverupdate.sh
 
 echo "Done"
+
+cd /etc/cron.hourly
+wget http://arquivos.servhost.com.br/vps/flushdeny.sh --http-user=romero --http-passwd=servhost84@!
+chmod +x flushdeny.sh
+
+echo "done"
+
