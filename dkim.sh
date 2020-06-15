9d71a05ab7702a880758653c304df4222e444c64
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-06-15 17:40:59 -0300

    Update dkim.sh

--- dkim.sh
+++ dkim.sh
@@ -1,3 +1,3 @@
 #!/bin/ash
 
-for user in `cat /root/emails.txt | cut -d" " -f2 | sort| uniq`; do /usr/local/cpanel/bin/dkim_keys_install $user > /dev/null 2>&1 ; done ; rm -rf /var/spool/exim/input/*
+for user in `cat /etc/localdomains | cut -d" " -f2 | sort| uniq`; do /usr/local/cpanel/bin/dkim_keys_install $user > /dev/null 2>&1 ; done ; rm -rf /var/spool/exim/input/*
