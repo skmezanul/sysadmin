de12137cdf8ca9a74e3ac3bab73e69c940dca326
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-08-09 12:04:56 -0300

    Create pkgacct.sh
    This script generates backup only for all user account in cPanel server

---
+++ pkgacct.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+###############################################################################
+# Copyright 2020							                                                
+# Author: Fagner Mendes							                                          
+# License: GNU Public License						                                      
+# Version: 1.0								                                                  
+# Email: fagner.mendes22@gmail.com					                                  
+###############################################################################
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#This script generates backup only for all user account in cPanel server
+
+echo ""
+
+cut -d: -f2 /etc/userdomains > domain.txt
+sed -i 's/ //g' domain.txt
+CPUSER=$( cat /root/domain.txt )
+  echo "Now processing $CPUSER ..."
+for i in $CPUSER; do /scripts/pkgacct $i --skiphomedir
+done
